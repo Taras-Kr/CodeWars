1164547b7e856ca4c195541b018b13db43de58b9
Taras Krasitskyi <you@example.com>
2022-03-15 16:09:58 +0200

    add SevenDataProvider

---
+++ src/test/java/com/codewars/kata/SevenDataProvider.java
@@ -0,0 +1,16 @@
+package com.codewars.kata;
+
+import org.testng.annotations.DataProvider;
+
+public class SevenDataProvider {
+
+    @DataProvider(name = "dpStringMerge")
+    public Object[][] dpStringMerge(){
+        return new Object[][]{
+                {"hello", "world", "l", "held"},
+                {"coding", "anywhere", "n", "codinywhere"},
+                {"jason", "samson", "s", "jasamson"},
+                {"wonderful", "people", "e", "wondeople"}
+        };
+    }
+}
