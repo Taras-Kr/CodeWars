5b201ef8742d9325dd7a238a0c7deb4bfad1b596
Taras Krasitskyi <you@example.com>
2022-03-18 11:48:58 +0200

    addd tests readInt

--- src/test/java/com/codewars/utils/ScannerDataProvider.java
+++ src/test/java/com/codewars/utils/ScannerDataProvider.java
@@ -4,11 +4,22 @@ import org.testng.annotations.DataProvider;
 
 public class ScannerDataProvider {
 
-    @DataProvider(name = "dpReadValidIntValid")
+    @DataProvider(name = "dpReadValidInt")
     public Object[][] dpReadValidInt(){
         return new Object[][]{
-                {"100", 100},
-                {"-100", -100}
+                {"-2147483648", -2147483648},
+                {"0", 0},
+                {"2147483647", 2147483647}
+        };
+    }
+
+    @DataProvider(name = "dpReadInvalidInt")
+    public Object[][] dpReadInvalidInt() {
+        return new Object[][]{
+                {"some_text\n1", "Bad integer number! Try again: "},
+                {"13658g2\n1","Bad integer number! Try again: " },
+                {"-2147483649\n1", "Bad integer number! Try again: "},
+                {"2147483648\n1", "Bad integer number! Try again: "}
         };
     }
 }
--- src/test/java/com/codewars/utils/ScannerTest.java
+++ src/test/java/com/codewars/utils/ScannerTest.java
@@ -4,16 +4,30 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 public class ScannerTest extends ScannerDataProvider{
 
-    @Test(dataProvider = "dpReadValidIntValid")
+    @Test(dataProvider = "dpReadValidInt")
     public void testReadValidInt(String number, int expected) {
         InputStream inputStream = new ByteArrayInputStream(number.getBytes());
         System.setIn(inputStream);
         ConsoleScanner consoleScanner = new ConsoleScanner();
         int actual = consoleScanner.readInt();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test (dataProvider = "dpReadInvalidInt")
+    public void testReadInvalidInt(String inputString, String expected){
+        InputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream (outputStream));
+        ConsoleScanner scanner = new ConsoleScanner();
+        scanner.readInt();
+        String actual = outputStream.toString();
         Assert.assertEquals(actual, expected);
     }
 }
